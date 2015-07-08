77bd5ce665a30a4ceb30308a421353057b6bbca0
J.C. Jones <jcjones@letsencrypt.org>
2015-07-08 18:49:11 +0000

    Rework to use a fetch/reset instead of a pull, so as to handle force pushes

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# vim: tw=120 ts=2:
 
 # Tell Pipes to die early
 set -o pipefail
@@ -13,54 +14,61 @@ run() {
   $*
 }
 
-REPO=~/letsencrypt/boulder
+REPO=${GOPATH}/src/github.com/letsencrypt/boulder
 SOURCE=~/
 DOCKER_REPO="quay.io/letsencrypt/boulder"
 
-BRANCH=$(cd ${REPO}; git symbolic-ref --short HEAD)
+BRANCH=${BRANCH:-master}
 LABEL=$(cd ${REPO}; git rev-parse --short HEAD)
 TAG=${DOCKER_REPO}:${LABEL}
 
 
 upgrade() {
-	cd ${REPO}
+  cd ${REPO}
 
-	r=$(git pull) 
-	if [ "Already up-to-date." == "$r" ] ; then die "$r" ; fi
+  OLDLABEL=$(cd ${REPO}; git rev-parse --short HEAD)
 
-	BRANCH=$(cd ${REPO}; git symbolic-ref --short HEAD)
-	LABEL=$(cd ${REPO}; git rev-parse --short HEAD)
-	TAG=${DOCKER_REPO}:${LABEL}
+  git fetch
+  git checkout ${BRANCH}  || die "Couldn't switch"
+  git reset --hard origin/${BRANCH} || die "Couldn't reset"
 
-	echo "Updated to ${LABEL}."
+  BRANCH=$(cd ${REPO}; git symbolic-ref --short HEAD)
+  LABEL=$(cd ${REPO}; git rev-parse --short HEAD)
+  TAG=${DOCKER_REPO}:${LABEL}
+
+  if [ "${OLDLABEL}" == "${LABEL}" ] ; then
+    die "${LABEL} is the latest version."
+  fi
+
+  echo "Updated to ${LABEL}."
 }
 
 compile() {
-	cd ${REPO}
-	make -j 9 || die "Failed"
+  cd ${REPO}
+  make -j 9 || die "Failed"
 }
 
 build() {
-	cd ${SOURCE}
+  cd ${SOURCE}
 
-	DEST=$(mktemp -d /tmp/build-dockerXXXX)
-	mkdir -p ${DEST}/etc ${DEST}/bin
-	cp Dockerfile ${DEST}
-	cp default-boulder-config.json ${DEST}/etc
-	cp ./gocode/src/github.com/letsencrypt/boulder/test/test-ca.pem ${DEST}/etc
-	cp ./gocode/src/github.com/letsencrypt/boulder/test/test-ca.key ${DEST}/etc
-	cp ~/letsencrypt/boulder/bin/* ${DEST}/bin
+  DEST=$(mktemp -d /tmp/build-dockerXXXX)
+  mkdir -p ${DEST}/etc ${DEST}/bin
+  cp Dockerfile ${DEST}
+  cp default-boulder-config.json ${DEST}/etc
+  cp ./gocode/src/github.com/letsencrypt/boulder/test/test-ca.pem ${DEST}/etc
+  cp ./gocode/src/github.com/letsencrypt/boulder/test/test-ca.key ${DEST}/etc
+  cp ~/letsencrypt/boulder/bin/* ${DEST}/bin
 
-	cd ${DEST}
+  cd ${DEST}
 
-	# TODO: Check if git is out of date
+  # TODO: Check if git is out of date
 
-	echo "Building ${TAG} ..."
-	run docker build -t ${TAG} .
-	echo "Built ${TAG}"
+  echo "Building ${TAG} ..."
+  run docker build -t ${TAG} .
+  echo "Built ${TAG}"
 
-	echo "Cleaning up..."
-	run rm -rf ${DEST}
+  echo "Cleaning up..."
+  run rm -rf ${DEST}
 }
 
 send() {
